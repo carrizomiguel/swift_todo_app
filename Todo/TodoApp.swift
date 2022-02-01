276ad79bad237becb05fa145c4d9b4e451819678
carrizomiguel <carrizo2000miguel@gmail.com>
2022-02-01 16:51:10 -0500

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by carrizane on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
