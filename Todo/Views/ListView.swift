b8b5b799d7ec0181709e1520ba467c68918225e1
carrizomiguel <carrizo2000miguel@gmail.com>
2022-02-12 20:00:12 -0500

    added NoItemsView in ListView

--- Todo/Views/ListView.swift
+++ Todo/Views/ListView.swift
@@ -12,19 +12,26 @@ struct ListView: View {
     @EnvironmentObject var listViewModel: ListViewModel
         
     var body: some View {
-        List {
-            ForEach(listViewModel.items) {
-                item in ListElementView(item: item)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            listViewModel.updateItem(item: item)
-                        }
+        ZStack {
+            if listViewModel.items.isEmpty {
+                NoItemsView()
+                    .transition(AnyTransition.opacity.animation(.easeIn))
+            } else {
+                List {
+                    ForEach(listViewModel.items) {
+                        item in ListElementView(item: item)
+                            .onTapGesture {
+                                withAnimation(.linear) {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
                     }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
+                .listStyle(PlainListStyle())
             }
-            .onDelete(perform: listViewModel.deleteItem)
-            .onMove(perform: listViewModel.moveItem)
         }
-        .listStyle(PlainListStyle())
         .navigationTitle("Todo List 📝")
         .navigationBarItems(
             leading: EditButton(),
---
+++ Todo/Views/NoItemsView.swift
@@ -0,0 +1,72 @@
+//
+//  NoItemsView.swift
+//  Todo
+//
+//  Created by carrizane on 2/12/22.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items!")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? Then add some activities to your Todo List!")
+                    .padding(.bottom, 20)
+                NavigationLink(
+                    destination: AddView(),
+                    label: {
+                        Text("Add items 🥳")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .frame(height: 55)
+                            .frame(maxWidth: .infinity)
+                            .background(animate ? secondaryAccentColor : Color.accentColor)
+                            .cornerRadius(10)
+                    })
+                    .padding(.horizontal, animate ? 30 : 50)
+                    .shadow(
+                        color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                        radius: animate ? 30 : 10,
+                        x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/,
+                        y: animate ? 20 : 10
+                    )
+                    .scaleEffect(animate ? 1.1 : 1.0)
+                    .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+struct NoItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            NoItemsView()
+                .navigationTitle("Title")
+        }
+    }
+}
